ac610a99ee7e3e9bf882ed3e9c4b8f564db42c92
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-01-18 21:01:06 +0100

    added script and patterns to inputs to validate

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
     const validationName = () => {
         const names = document.querySelectorAll(".js-name");
         const lettersRegex = /^[A-Za-z]+$/;
-    
+
         names.forEach(name => {
             name.addEventListener("blur", () => {
                 const nameValue = name.value;
-                if(!lettersRegex.test(nameValue)){
+                if (!lettersRegex.test(nameValue)) {
                     alert("Imię i nazwisko musi zawierać tylko litery!");
                     name.value = "";
                 };
@@ -14,10 +14,23 @@
         });
     };
 
+    const consoleLog = () => {
+        console.log("Cześć, skrypt działa. Zablokowane jest wysyłanie formularza :)")
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        consoleLog();
+      };
+
     const init = () => {
         validationName();
+        
+        const form = document.querySelector(".js-form");
 
+        form.addEventListener("submit", onFormSubmit);
     };
-    
+
     init();
 }    
